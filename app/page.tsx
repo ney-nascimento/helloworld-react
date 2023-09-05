7fd704a084f0bca0578627acd916800b937c47db
Ney Nascimento <135929048+ney-nascimento@users.noreply.github.com>
2023-09-05 16:48:27 -0300

    next-APRENDENDO

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+<header>
+    <h1 className=' text-green-500 p-4 '>Eliti quero entrar</h1>
+   <nav className='text-green-500'>
+    <ul>
+       <a href='pages.tsx'>exemplo</a>
+    </ul>
+   </nav>
+ </header>
+  )
+}
